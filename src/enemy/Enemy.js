b3d0a58b9da77feacdff55fa21559d2432ccbdec
minimo <minimo.mobile@gmail.com>
2014-09-15 16:53:59 +0900

    update point diaplay

--- src/enemy/Enemy.js
+++ src/enemy/Enemy.js
@@ -159,7 +159,8 @@ tm.define("pb3.Enemy", {
 
             //得点表示
             var sc = tm.display.OutlineLabel(this.data.point, 30).addChildTo(this.parentScene).setPosition(this.x, this.y);
-            sc.fontFamily = "'UbuntuMono'"; sc.align = "center"; sc.baseline  = "middle"; sc.fontWeight = 300; sc.outlineWidth = 2;
+            sc.layer = LAYER_FOREGROUND;
+            sc.setParam({fontFamily:"'UbuntuMono'", align: "center", baseline:"middle", fontWeight:300, outlineWidth:2 });
             sc.tweener.to({x: this.x, y: this.y-50, alpha:0}, 1000).call(function(){this.remove()}.bind(sc));
         }
     },
