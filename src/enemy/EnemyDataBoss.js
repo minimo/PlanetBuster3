15d8f47f200c69621b236bb43b1fec32fdf35ac5
minimo <minimo.mobile@gmail.com>
2015-08-28 12:17:41 +0900

    update stage1 boss

--- src/enemy/EnemyDataBoss.js
+++ src/enemy/EnemyDataBoss.js
@@ -39,8 +39,8 @@ pb3.enemyData['ThorHammer'] = {
 
     //機体用テクスチャ情報
     texName: "boss1",
-    texWidth: 32,
-    texHeight: 32,
+    texWidth: 96,
+    texHeight: 192,
     texIndex: 0,
 
     setup: function(enterParam) {
@@ -57,6 +57,151 @@ pb3.enemyData['ThorHammer'] = {
  *  局地制圧型巨大戦車「ゴリアテ」
  *
  */
+//本体
+pb3.enemyData['Golyat'] = {
+    //使用弾幕パターン
+    bulletPattern: "Golyat",
+
+    //当り判定サイズ
+    width:  64,
+    height: 64,
+
+    //耐久力
+    def: 1000,
+
+    //得点
+    point: 10000,
+
+    //表示レイヤー番号
+    layer: LAYER_OBJECT_LOWER,
+
+    //敵タイプ
+    type: ENEMY_BOSS,
+
+    //爆発タイプ
+    explodeType: EXPLODE_MBOSS,
+
+    //機体用テクスチャ情報
+    texName: "boss1",
+    texWidth: 32,
+    texHeight: 32,
+    texTrimX: 192,
+    texTrimY: 0,
+    texTrimWidth: 184,
+    texTrimHeight: 184,
+    texIndex: 0,
+
+    setup: function(enterParam) {
+        this.phase = 0;
+        this.isCollision = false;
+        this.isGround = true;
+
+        //ボディカバー
+        this.cover = tm.display.Sprite("boss1", 32, 32)
+            .addChildTo(this)
+            .setPosition(0, 0)
+            .setFrameTrimming(382, 0, 32, 32);
+
+        this.core = tm.display.Sprite("boss1", 32, 32)
+            .addChildTo(this)
+            .setPosition(0, 0)
+            .setFrameTrimming(0, 0, 32, 32);
+
+        //登場パターン
+        this.tweener.clear()
+            .move(SC_W*0.5, SC_H*-0.5, 3000, "easeOutSine")
+            .call(function(){this.phase++;}.bind(this));
+    },
+
+    algorithm: function() {
+        if (this.phase == 2) {
+            this.isCollision = true;
+            this.phase++;
+        }
+    },
+};
+
+//アーム
+pb3.enemyData['GolyatArm'] = {
+    //使用弾幕パターン
+    bulletPattern: "",
+
+    //当り判定サイズ
+    width:  64,
+    height: 64,
+
+    //耐久力
+    def: 500,
+
+    //得点
+    point: 10000,
+
+    //表示レイヤー番号
+    layer: LAYER_OBJECT_LOWER,
+
+    //敵タイプ
+    type: ENEMY_BOSS,
+
+    //爆発タイプ
+    explodeType: EXPLODE_MBOSS,
+
+    //機体用テクスチャ情報
+    texName: "boss1",
+    texWidth: 32,
+    texHeight: 32,
+    texTrimX: 192,
+    texTrimY: 0,
+    texTrimWidth: 184,
+    texTrimHeight: 184,
+    texIndex: 0,
+
+    setup: function(enterParam) {
+    },
+
+    algorithm: function() {
+    },
+};
+
+//砲台
+pb3.enemyData['GolyatTurret'] = {
+    //使用弾幕パターン
+    bulletPattern: "",
+
+    //当り判定サイズ
+    width:  64,
+    height: 64,
+
+    //耐久力
+    def: 500,
+
+    //得点
+    point: 10000,
+
+    //表示レイヤー番号
+    layer: LAYER_OBJECT_LOWER,
+
+    //敵タイプ
+    type: ENEMY_BOSS,
+
+    //爆発タイプ
+    explodeType: EXPLODE_MBOSS,
+
+    //機体用テクスチャ情報
+    texName: "boss1",
+    texWidth: 32,
+    texHeight: 32,
+    texTrimX: 192,
+    texTrimY: 0,
+    texTrimWidth: 184,
+    texTrimHeight: 184,
+    texIndex: 0,
+
+    setup: function(enterParam) {
+    },
+
+    algorithm: function() {
+    },
+};
 
 /*
  *
